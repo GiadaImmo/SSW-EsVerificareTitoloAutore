cfe184ebf91424cfa0e6472c3ee27d1045cfaed2
GiadaImmo <120042391+GiadaImmo@users.noreply.github.com>
2023-03-06 15:40:25 +0100

    Lezione_01-03

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+/*
+  Provate a scrivere il codice per verificare titolo e autore di un certo volume contengono una certa sequenza di caratteri:
+    - concatenate titolo e autore
+    - convertite le maiuscole in minuscole
+    - verificate la presenza della stringa
+  I tre elementi (autore, titolo e stringa) possono essere contenuti in tre
+  variabili. Cercate di scrivere un’unica riga per i tre passi precedenti,
+  componendo le funzioni con il punto (.). Non utilizzate form per
+  l’inserimento dei parametri, ma impostate i valori nelle variabili.
+*/
+
 // Import stylesheets
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let autore = 'Dante Alighieri'; // Nome Autore
+let titolo = 'La Divina Commedia'; // Titolo Libro
+let stringa = 'di'; // Ciò che inserisce l'utente
+
+if ((autore + ' ' + titolo).toLowerCase().includes(stringa.toLowerCase())) {
+  // alert(autore + ' "' + titolo + '"');
+  // console.log(autore + ' "' + titolo + '"')
+  document.getElementById('risultatoRicerca').innerHTML =
+    autore + ' "' + titolo + '"';
+} else {
+  // alert('Autore e Libro non trovati');
+  // console.log("Autore e Libro non trovati");
+  document.getElementById('risultatoRicerca').innerHTML =
+    'Autore e Libro non trovati';
+}
